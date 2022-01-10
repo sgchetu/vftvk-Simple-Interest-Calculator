4bb1c90714a8404d62044799a4ff61c13866b586
sgchetu <96946308+sgchetu@users.noreply.github.com>
2022-01-10 11:27:19 +0530

    Update script.js
    Added function calls

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
 function compute()
 {
-    var principal = document.getElementById("principal").value;
+
+var principal = document.getElementById("principal").value;
+
+function myFunction() {
+    // If amount/principal is Not a Number or less than one;
+        if (isNaN(principal) || principal < 0 ||principal=='') {
+        alert("Please enter a positive amount");
+        document.getElementById("result").innerHTML='';
+        fail;
+  } else {
+        updateRate() ;
+    
+  }
+
+  }
+
     var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = principal * years * rate /100;
-    var year = new Date().getFullYear()+parseInt(years);
+
     function updateRate() 
         {
             var rateval = document.getElementById("rate").value;
-            document.getElementById("rate_val").innerText=rateval;
+            document.getElementById("rate_val").innerText=rateval+"%";
+
+
         }  
+
+
+    myFunction() ;
+ 
+    var years = document.getElementById("years").value;
+    var interest = (principal * years * rate) /100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    var amount=+principal+ +interest;
         
-     document.getElementById("result").innerHTML="If you deposit " +principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+    document.getElementById("result").innerHTML="\<br\>\<br\>If you deposit "+"<mark>"+principal+","+"</mark>"+"\<br\> at the interest rate "+"<mark>"+rate+"%."+"</mark>"+" \<br\> You will receive an amount of "+"<mark>"+amount+"</mark>"+"\<br\>in the year "+"<mark>"+year+"</mark>"+".";
+
      function setFocusToTextBox(){
      document.getElementById("principal").focus();
     }
+    setFocusToTextBox();
 
     
 }
-           
+        
