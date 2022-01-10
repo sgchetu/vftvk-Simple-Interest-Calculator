35bdc421c0ad51e614e5d327deb30d25c5ac1fd7
sgchetu <96946308+sgchetu@users.noreply.github.com>
2022-01-10 13:04:40 +0530

    Update script.js
    Changed Principal amount validation condition

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var principal = document.getElementById("principal").value;
 
 function myFunction() {
     // If amount/principal is Not a Number or less than one;
-        if (isNaN(principal) || principal < 0 ||principal=='') {
+        if (isNaN(principal) || principal < 1 ||principal=='') {
         alert("Please enter a positive amount");
         document.getElementById("result").innerHTML='';
         fail;
